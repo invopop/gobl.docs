f83ce5932fc11c039013706b599d23535deaad04
Sam Lown <sam.lown@invopop.com>
2022-03-22 11:10:08 +0000

    Working on auto generator idea

---
+++ internal/generator.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"github.com/invopop/jsonschema"
+)
+
+// Generate takes the json schemas defined at the provided directory
+// and attempts to create a set of markdown documents.
+func Generate(path string) error {
+
+	// Open the directory at the provided path
+
+	// Go through each JSON Schema document
+
+	// Parse as our jsonschema Schema model
+
+	// Output as markdown
+
+	return nil
+}
+
+func processSchema(schema *jsonschema.Schema) error {
+
+	return nil
+}
